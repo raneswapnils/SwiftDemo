82cf0f887379e4dbde876b6f93d063f20a7ae31f
Swapnil Rane <SwapnilRane@devaccorapps.com>
2024-07-31 11:08:29 +0530

    Added new project

---
+++ TestDemo/TestDemo/TestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDemoApp.swift
+//  TestDemo
+//
+//  Created by Swapnil Rane on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
